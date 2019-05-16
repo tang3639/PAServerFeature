7543cccbe21823b9d141a8298b3132b538413104
MiunaPA <fish126732@gmail.com>
2019-05-17 00:51:04 +0800

    重整後第一次commit

---
+++ src/main/java/me/miuna/paserverfeature/DispensePlanting.java
@@ -0,0 +1,132 @@
+package me.miuna.paserverfeature;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.event.Listener;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Dispenser;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+public class DispensePlanting implements Listener {
+
+    @EventHandler
+    public void onDispenseEvent(BlockDispenseEvent event) {
+        Block block = event.getBlock();
+        Material selectSeed = detectSelf(block);
+        if (selectSeed != null) {
+            Character mode = detectModeBlock(block);
+            if (mode != 'N') {
+                farming(block, selectSeed, mode);
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    // 檢查發射器是否朝下 & 是否有種子
+    public Material detectSelf(Block block) {
+        Inventory inventory = ((Dispenser) block.getState()).getInventory();
+        BlockFace blockFace =
+                ((org.bukkit.material.Dispenser) block.getState().getData()).getFacing();
+        if ((blockFace == BlockFace.DOWN)) {
+            List<Material> itemList = new ArrayList<Material>();
+            itemList = seedList(itemList);
+            Material selectSeed = null;
+            for (Material item : itemList) {
+                if (inventory.contains(item)) {
+                    selectSeed = item;
+                    break;
+                }
+            }
+            return selectSeed;
+        } else {
+            return null;
+        }
+    }
+
+    // 種子列表
+    public List<Material> seedList(List<Material> itemList) {
+        itemList.add(Material.WHEAT_SEEDS);
+        itemList.add(Material.POTATO);
+        itemList.add(Material.CARROT);
+        itemList.add(Material.BEETROOT_SEEDS);
+        return itemList;
+    }
+
+    // 檢查發射器上方的模式方塊
+    public Character detectModeBlock(Block block) {
+        Location upLocation =
+                new Location(block.getWorld(), block.getX(), block.getY() + 2, block.getZ());
+        Block upBlock = upLocation.getBlock();
+        if (upBlock.getType() == Material.HAY_BLOCK) {
+            return 'S';
+        } else if (upBlock.getType() == Material.IRON_BLOCK) {
+            return 'M';
+        } else if (upBlock.getType() == Material.DIAMOND_BLOCK) {
+            return 'L';
+        } else {
+            return 'N';
+        }
+    }
+
+    // 種植種子
+    public void farming(Block block, Material selectSeed, Character mode) {
+        int min = 0;
+        int max = 0;
+        if (mode == 'S') {
+            min = 0;
+            max = 1;
+        } else if (mode == 'M') {
+            min = -1;
+            max = 2;
+        } else if (mode == 'L') {
+            min = -2;
+            max = 3;
+        }
+        Material seedBlock = null;
+        if (selectSeed == Material.WHEAT_SEEDS) {
+            seedBlock = Material.WHEAT;
+        } else if (selectSeed == Material.POTATO) {
+            seedBlock = Material.POTATOES;
+        } else if (selectSeed == Material.CARROT) {
+            seedBlock = Material.CARROTS;
+        } else if (selectSeed == Material.BEETROOT_SEEDS) {
+            seedBlock = Material.BEETROOTS;
+        }
+        Dispenser dispenser = (Dispenser) block.getState();
+        Inventory inventory = dispenser.getInventory();
+        for (int i = min; i < max; i++) {
+            for (int j = min; j < max; j++) {
+                Location groundLocation = new Location(block.getWorld(), block.getX() + i,
+                        block.getY() - 3, block.getZ() + j);
+                Location seedLocation = new Location(block.getWorld(), block.getX() + i,
+                        block.getY() - 2, block.getZ() + j);
+                if ((groundLocation.getBlock().getType() == Material.FARMLAND)
+                        && (seedLocation.getBlock().getType() == Material.AIR)) {
+                    for (ItemStack item : inventory.getContents()) {
+                        if (item != null) {
+                            if (item.getType() == selectSeed) {
+                                item.setAmount(item.getAmount() - 1);
+                                seedLocation.getBlock().setType(seedBlock);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        dispenser.update();
+    }
+
+    public DispensePlanting() {
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(this, pm.getPlugin("PAServerFeature"));
+    }
+}
---
+++ src/main/java/me/miuna/paserverfeature/Main.java
@@ -0,0 +1,19 @@
+package me.miuna.paserverfeature;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        init();
+        getLogger().info("PAServerFeature 啟動  Author:MiunaPA");
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public void init() {
+        new DispensePlanting();
+    }
+}
