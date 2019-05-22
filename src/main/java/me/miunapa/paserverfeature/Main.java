98e020ade820edfac80d30aca84fe5749976d664
MiunaPA <fish126732@gmail.com>
2019-05-22 20:59:19 +0800

    增加/paf重新讀取功能

--- src/main/java/me/miunapa/paserverfeature/Main.java
+++ src/main/java/me/miunapa/paserverfeature/Main.java
@@ -1,16 +1,37 @@
 package me.miunapa.paserverfeature;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements CommandExecutor {
     @Override
     public void onEnable() {
         init();
         getLogger().info("PAServerFeature 啟動  Author:MiunaPA");
-    }
+        }
 
     @Override
     public void onDisable() {
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equals("paf")) {
+            if (args.length >= 1) {
+                if (args[0].equals("reload")) {
+                    init();
+                    if (sender instanceof Player) {
+                        sender.sendMessage("§aPAServerFeature重新讀取完成");
+                    } else {
+                        System.out.println("PAServerFeature重新讀取完成");
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     public void init() {
