9be3d0b3e708a47f92a5591871e3350e87266bfb
MiunaPA <fish126732@gmail.com>
2019-06-06 18:22:42 +0800

    增加釣到時println

--- src/main/java/me/miunapa/paserverfeature/Fishing.java
+++ src/main/java/me/miunapa/paserverfeature/Fishing.java
@@ -8,6 +8,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import net.md_5.bungee.api.ChatColor;
 
 public class Fishing extends Feature implements Listener {
     @EventHandler
@@ -17,6 +18,8 @@ public class Fishing extends Feature implements Listener {
             if (itemStack.getType() == Material.ENCHANTED_BOOK) {
                 EnchantmentStorageMeta meta = (EnchantmentStorageMeta) itemStack.getItemMeta();
                 if (meta.getEnchants().containsKey(Enchantment.MENDING)) {
+                    System.out.println(event.getPlayer().getName() + ChatColor.GOLD
+                            + " get Mending EnchantmentBook");
                     event.setCancelled(true);
                 } ;
             } ;
