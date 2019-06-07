28c157331c983923b1a9d97b9a5e78baec8b95ae
MiunaPA <fish126732@gmail.com>
2019-06-07 11:43:17 +0800

    修復剝皮原木無法放置的問題

--- src/main/java/me/miunapa/paserverfeature/BlockPlace.java
+++ src/main/java/me/miunapa/paserverfeature/BlockPlace.java
@@ -9,30 +9,39 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import net.md_5.bungee.api.ChatColor;
 
 public class BlockPlace extends Feature implements Listener {
-    List<Material> trippedLog = new ArrayList<Material>();
+    List<Material> trippedLogs = new ArrayList<Material>();
+    List<Material> axes = new ArrayList<Material>();
 
     @EventHandler
     public void onTrippedLog(BlockPlaceEvent event) {
-        if (trippedLog.contains(event.getBlock().getType())) {
-            event.getPlayer().sendMessage(
-                    ChatColor.GOLD + "若要取得剝皮原木" + ChatColor.WHITE + " 請將原木or木塊以壓力版合成方式取得");
-            event.setCancelled(true);
+        if (trippedLogs.contains(event.getBlock().getType())) {
+            Material playerTool = event.getPlayer().getInventory().getItemInMainHand().getType();
+            if (axes.contains(playerTool)) {
+                event.getPlayer().sendMessage(
+                        ChatColor.GOLD + "若要取得剝皮原木" + ChatColor.WHITE + " 請將原木or木塊 排成同壓力版合成方式 取得");
+                event.setCancelled(true);
+            }
         }
     }
 
     public void trippedLogListInit() {
-        trippedLog.add(Material.STRIPPED_ACACIA_LOG);
-        trippedLog.add(Material.STRIPPED_ACACIA_WOOD);
-        trippedLog.add(Material.STRIPPED_BIRCH_LOG);
-        trippedLog.add(Material.STRIPPED_BIRCH_WOOD);
-        trippedLog.add(Material.STRIPPED_DARK_OAK_LOG);
-        trippedLog.add(Material.STRIPPED_DARK_OAK_WOOD);
-        trippedLog.add(Material.STRIPPED_JUNGLE_LOG);
-        trippedLog.add(Material.STRIPPED_JUNGLE_WOOD);
-        trippedLog.add(Material.STRIPPED_OAK_LOG);
-        trippedLog.add(Material.STRIPPED_OAK_WOOD);
-        trippedLog.add(Material.STRIPPED_SPRUCE_LOG);
-        trippedLog.add(Material.STRIPPED_SPRUCE_WOOD);
+        trippedLogs.add(Material.STRIPPED_ACACIA_LOG);
+        trippedLogs.add(Material.STRIPPED_ACACIA_WOOD);
+        trippedLogs.add(Material.STRIPPED_BIRCH_LOG);
+        trippedLogs.add(Material.STRIPPED_BIRCH_WOOD);
+        trippedLogs.add(Material.STRIPPED_DARK_OAK_LOG);
+        trippedLogs.add(Material.STRIPPED_DARK_OAK_WOOD);
+        trippedLogs.add(Material.STRIPPED_JUNGLE_LOG);
+        trippedLogs.add(Material.STRIPPED_JUNGLE_WOOD);
+        trippedLogs.add(Material.STRIPPED_OAK_LOG);
+        trippedLogs.add(Material.STRIPPED_OAK_WOOD);
+        trippedLogs.add(Material.STRIPPED_SPRUCE_LOG);
+        trippedLogs.add(Material.STRIPPED_SPRUCE_WOOD);
+        axes.add(Material.WOODEN_AXE);
+        axes.add(Material.STONE_AXE);
+        axes.add(Material.IRON_AXE);
+        axes.add(Material.GOLDEN_AXE);
+        axes.add(Material.DIAMOND_AXE);
     }
 
     public BlockPlace() {
