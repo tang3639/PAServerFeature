71fb0766cacfafa6b99312ae0b3a47da29d98982
MiunaPA <edge0945126732>
2019-12-03 19:01:10 +0800

    移除command import

--- src/main/java/me/miunapa/paserverfeature/FeatureStart.java
+++ src/main/java/me/miunapa/paserverfeature/FeatureStart.java
@@ -1,11 +1,7 @@
 package me.miunapa.paserverfeature;
 
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import me.miunapa.paserverfeature.block.*;
