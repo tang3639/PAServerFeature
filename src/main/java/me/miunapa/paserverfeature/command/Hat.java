f4fb4b18184c8d69da270a9823180d602e070163
MiunaPA <fish126732@gmail.com>
2019-12-16 01:14:16 +0800

    戴帽子回傳文字改以actionbar顯示

--- src/main/java/me/miunapa/paserverfeature/command/Hat.java
+++ src/main/java/me/miunapa/paserverfeature/command/Hat.java
@@ -7,43 +7,34 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Material;
 import org.bukkit.Bukkit;
 
-
 public class Hat implements CommandExecutor {
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        String prefix = ChatColor.LIGHT_PURPLE + "[" + ChatColor.AQUA + "PA-Hat"
-                + ChatColor.LIGHT_PURPLE + "] " + ChatColor.RESET;
         if (sender instanceof Player) {
             Player player = (Player) sender;
             PlayerInventory inv = player.getInventory();
             ItemStack hand = inv.getItemInMainHand();
             ItemStack hat = inv.getHelmet();
             if (hand.getType() == Material.AIR) {
-                player.sendMessage(prefix + ChatColor.GRAY + "你手上沒有東西...");
+                player.spigot().sendMessage(ChatMessageType.ACTION_BAR,
+                        TextComponent.fromLegacyText(ChatColor.GRAY + "你手上沒有東西..."));
                 return true;
             }
             if (hand.getAmount() != 1) {
-                player.sendMessage(prefix + ChatColor.RED + "你手上物品數量超過一個 無法穿上");
-                return true;
-            }
-            if (hat != null && hat.getType() != Material.AIR) {
-                if (hat.getItemMeta().getDisplayName().equals("§e西伯利亞礦工帽")) {
-                    player.sendMessage(prefix + ChatColor.RED + "請先脫下礦工帽再使用");
-                    return true;
-                }
-            }
-            if (hand.getItemMeta().getDisplayName().equals("§e西伯利亞礦工帽")) {
-                player.sendMessage(prefix + ChatColor.RED + "不能使用此指令穿上礦工帽");
+                player.spigot().sendMessage(ChatMessageType.ACTION_BAR,
+                        TextComponent.fromLegacyText(ChatColor.RED + "你手上物品數量超過一個 無法穿上"));
                 return true;
             }
             inv.setHelmet(hand);
             inv.setItemInMainHand(hat);
             player.updateInventory();
-            player.sendMessage(prefix + ChatColor.GREEN + "帽子穿好囉~");
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR,
+                    TextComponent.fromLegacyText(ChatColor.GREEN + "帽子穿好囉~"));
         }
         return true;
     }
