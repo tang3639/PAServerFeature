5b6030257ee40110f4042477aaa88fa3159c5864
MiunaPA <fish126732@gmail.com>
2020-01-10 18:28:00 +0800

    修改function名稱

--- src/main/java/me/miunapa/pamainserverfeature/entity/IronGlormSpawn.java
+++ src/main/java/me/miunapa/pamainserverfeature/entity/IronGlormSpawn.java
@@ -10,7 +10,7 @@ import me.miunapa.pamainserverfeature.FeatureStart;
 public class IronGlormSpawn extends FeatureStart implements Listener {
 
     @EventHandler
-    public void mobSpawn(CreatureSpawnEvent event) {
+    public void ironGlormSpawnEvent(CreatureSpawnEvent event) {
         if (event.getEntityType().equals(EntityType.IRON_GOLEM)) {
             Double random = Math.random();
             if (random > 0.2 && event.getSpawnReason() == SpawnReason.VILLAGE_DEFENSE) {
