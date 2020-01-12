9f77889757a455f3fefab8a8ba94ba446d3946fe
MiunaPA <fish126732@gmail.com>
2020-01-12 18:06:58 +0800

    將鐵巨人生成控制改為可自訂

--- src/main/java/me/miunapa/pamainserverfeature/entity/IronGlormSpawn.java
+++ src/main/java/me/miunapa/pamainserverfeature/entity/IronGlormSpawn.java
@@ -1,25 +1,35 @@
 package me.miunapa.pamainserverfeature.entity;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.plugin.Plugin;
 import me.miunapa.pamainserverfeature.FeatureStart;
 
 public class IronGlormSpawn extends FeatureStart implements Listener {
+    Plugin plugin = Bukkit.getPluginManager().getPlugin("pamainserverfeature");
+    FileConfiguration config = plugin.getConfig();
 
     @EventHandler
     public void ironGlormSpawnEvent(CreatureSpawnEvent event) {
         if (event.getEntityType().equals(EntityType.IRON_GOLEM)) {
             Double random = Math.random();
-            if (random > 0.2 && event.getSpawnReason() == SpawnReason.VILLAGE_DEFENSE) {
+            Double chance = config.getDouble("IronGlormSpawn");
+            if (random > chance && event.getSpawnReason() == SpawnReason.VILLAGE_DEFENSE) {
                 event.setCancelled(true);
             }
         }
     }
 
     public IronGlormSpawn() {
-        pm.registerEvents(this, pm.getPlugin("pamainserverfeature"));
+        pm.registerEvents(this, plugin);
+        plugin.getConfig().addDefault("IronGlormSpawn", 0.2);
+        plugin.getConfig().options().copyDefaults(true);
+        plugin.saveConfig();
+        plugin.reloadConfig();
     }
 }
