8219a69000d16b99f8ff77e1c78fdfb2b78c3541
MiunaPA <fish126732@gmail.com>
2020-04-22 21:58:50 +0800

    完成使用棒子選取

--- src/main/java/me/miunapa/paserverfeature/feature/ArmorStandEditor.java
+++ src/main/java/me/miunapa/paserverfeature/feature/ArmorStandEditor.java
@@ -2,7 +2,11 @@ package me.miunapa.paserverfeature.feature;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
@@ -16,23 +20,41 @@ import org.bukkit.inventory.meta.ItemMeta;
 import me.miunapa.paserverfeature.FeatureStart;
 import net.md_5.bungee.api.ChatColor;
 
-public class ArmorStandEditor extends FeatureStart implements Listener {
+public class ArmorStandEditor extends FeatureStart implements Listener, CommandExecutor {
 
     @EventHandler
     public void click(PlayerInteractAtEntityEvent event) {
         Player player = event.getPlayer();
         if (event.getRightClicked().getType().equals(EntityType.ARMOR_STAND)) {
             ArmorStand armorStand = (ArmorStand) event.getRightClicked();
-            player.sendMessage(armorStand.toString());
+            if (testItem(player.getInventory().getItemInMainHand())) {
+                player.sendMessage(armorStand.toString());
+            } ;
         } ;
     }
 
-    public ArmorStandEditor() {
-        pm.registerEvents(this, plugin);
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (args.length == 0) {
+                player.sendMessage(ChatColor.GRAY + "(ArmorStandEditor)" + ChatColor.RED + "請輸入參數");
+            } else {
+                if (args[0].equals("get")) {
+                    getArmorStandEditorItem(player);
+                    player.sendMessage(ChatColor.GREEN + "已給予盔甲架編輯用棒棒");
+                } else {
+                    player.sendMessage(ChatColor.RED + "目前的參數有:get");
+                }
+            }
+        }
+        return true;
     }
 
+
+
     boolean testItem(ItemStack item) {
-        if (!(item.getType() == Material.LANTERN)) {
+        if (!(item.getType() == Material.STICK)) {
             return false;
         }
         ItemMeta meta = item.getItemMeta();
@@ -58,5 +80,10 @@ public class ArmorStandEditor extends FeatureStart implements Listener {
         itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(itemMeta);
         player.getInventory().addItem(item);
+    }
+
+    public ArmorStandEditor() {
+        pm.registerEvents(this, plugin);
+        Bukkit.getPluginCommand("aseditor").setExecutor(this);
     }
 }
